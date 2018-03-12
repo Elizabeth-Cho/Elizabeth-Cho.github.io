7501c0822cabc0ac784a7c65ec6f8525b9fa476e
Elizabeth Cho <zergling13rush@gmail.com>
2018-03-11 22:19:04 -0400

    Initial Project Firestorm about page

---
+++ assets/js/tabs.js
@@ -0,0 +1,18 @@
+function openChar(charName) {
+    var i;
+    var x = document.getElementsByClassName("char");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(charName).style.display = "inline-block";
+}
+
+function openList(){
+  var x = document.getElementById("char-bar");
+  if(x.className === "char-tab"){
+    x.clasName += " responsive";
+  }
+  else{
+    x.className = "char-tab";
+  }
+}
---
+++ firestorm/about/index.md
@@ -0,0 +1,4 @@
+---
+layout: firestormAbout
+title: ''
+---
