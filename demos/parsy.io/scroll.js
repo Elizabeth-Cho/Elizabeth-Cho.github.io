b7d035a9bf0af252dc6c1a23e76d10d87f04584b
Elizabeth Cho <zergling13rush@gmail.com>
2019-07-29 21:49:22 -0400

    yay chameleon

--- demos/parsy.io/scroll.js
+++ demos/parsy.io/scroll.js
@@ -1,11 +1,14 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementsByClassName("navbar").style.background =
-        "rgba(0, 0, 0, 0)";
-  } else {
-    document.getElementsByClassName("navbar").style.background =
-        "rgba(0, 0, 0, 1)";
-  }
+window.onscroll = function() {
+    var nav = document.getElementsByClassName("navbar");
+    if (window.pageYOffset > 200) {
+        nav.classList.add("navbar-add");
+    }
+    else {
+        nav.classList.remove("navbar-add");
+    }
 }
+
+const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    speedAsDuration: true
+});
